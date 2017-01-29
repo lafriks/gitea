63a7485e8a3685092a4ff08adc7d7f9afd162996
Stanislav Sokolov <sokolov.stas@gmail.com>
2017-01-29 22:59:44 +0500

    Added showing code lines in comments

--- routers/routes/routes.go
+++ routers/routes/routes.go
@@ -552,7 +552,7 @@ func RegisterRoutes(m *macaron.Macaron) {
 			m.Get("/^:type(issues|pulls)$", repo.RetrieveLabels, repo.Issues)
 			m.Get("/^:type(issues|pulls)$/:index", repo.ViewIssue)
 			m.Get("/reviews", repo.RetrieveLabels, repo.Reviews)
-			m.Get("/reviews/:index", repo.ViewReview)
+			m.Get("/reviews/:index", repo.SetEditorconfigIfExists, repo.ViewReview)
 			m.Get("/labels/", repo.RetrieveLabels, repo.Labels)
 			m.Get("/milestones", repo.Milestones)
 		}, context.RepoRef(), context.CheckUnit(models.UnitTypeIssues))
